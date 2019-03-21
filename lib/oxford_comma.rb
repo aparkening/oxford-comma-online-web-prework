dee9a7c212307c7e84118e2cd8bdf70c135dd976
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 22:20:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
     string = array.join
   elsif array.length == 2
-    string = array.join("and ")
+    string = array.join(" and ")
   elsif array.length > 2
     new_last = "and #{array.last}"
     array.pop
