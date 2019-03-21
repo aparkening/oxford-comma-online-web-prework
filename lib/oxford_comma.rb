b30ee56ba6269293c66640ddae8db3accf97de70
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 22:17:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.join
+  elsif array.length == 2
+    string = array.join("and ")
+  elsif array.length > 2
+    array[last] = "and #{array.last}"
+    string = array.join(", ")
+  else 
+    string = "No array here!"
+  end
+  return string
 end
